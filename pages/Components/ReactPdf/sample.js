0f7b2d96ec61211da8a847a1579813a879abf35a
ryuyaF <r.fukushima403@gmail.com>
2022-11-24 02:47:23 +0900

    react-pdfのサンプルを作成

---
+++ pages/Components/ReactPdf/sample.js
@@ -0,0 +1,107 @@
+import { Page, Text, Document, PDFViewer, StyleSheet, Font, Note } from '@react-pdf/renderer';
+
+Font.register({
+  family: 'Nasu-Regular',
+  src: "./fonts/Nasu-Regular.ttf",
+})
+
+// Create styles
+const styles = StyleSheet.create({
+  viewer: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+  document: {
+    fontFamily: 'Nasu-Regular'
+  },
+  header: {
+    fontSize: 24,
+    textAlign: 'center',
+    color: 'orange',
+    backgroundColor: 'blue',
+  },
+  content: {
+  },
+  pageCounter: {
+    position: 'absolute',
+    fontSize: 12,
+    bottom: 30,
+    left: 0,
+    right: 0,
+    textAlign: 'center',
+    color: 'orange',
+    backgroundColor: 'gray',
+  },
+});
+
+const Header = ({ text }) => (
+    <Text style={styles.header} fixed>{text ?? 'これはヘッダーです'}</Text>
+)
+
+const PageCounter = () => {
+  const handleRender = ({ pageNumber, totalPages }) => `${pageNumber} / ${totalPages}`
+  return <Text style={styles.pageCounter} render={handleRender} fixed />
+}
+
+const CustomPage = (props) => {
+  return <Page {...props}>
+    <Header text={props.headerText} />
+      {props.children}
+    <PageCounter />
+  </Page>
+}
+
+const Content = () => (
+  <Document style={styles.document}
+    title='サンプルタイトル'
+  >
+    {/* A4縦 */}
+    <CustomPage
+      headerText='A4縦'
+    >
+      <Text>A4縦</Text>
+    </CustomPage>
+
+    {/* A4横 */}
+    <CustomPage
+      headerText='A4横'
+      orientation='landscape'
+    >
+      <Text>A4横</Text>
+    </CustomPage>
+
+    {/* 複数ページ */}
+    <CustomPage
+      headerText='複数ページ'
+    >
+      {[...Array(50)].map((_, i) => {
+        return <Text key={i}>Text #{i + 1}</Text>
+      })}
+    </CustomPage>
+
+    {/* 複数ページ(10行ごとに改行あり) */}
+    <CustomPage
+      headerText='複数ページ(10行ごとに改行あり)'
+    >
+      {[...Array(50)].map((_, i) => {
+        return <Text key={i} break={i != 0 && i % 10 == 0}>Text #{i + 1}</Text>
+      })}
+    </CustomPage>
+  </Document>
+)
+
+const Sample = ({ isPreview = false }) => {
+
+  if (isPreview) return (
+    <PDFViewer
+      style={styles.viewer}
+    >
+      <Content />
+    </PDFViewer>
+  )
+
+  return (
+    <Content />
+  )
+}
+export default Sample
---
+++ pages/react-pdf-sample/index.js
@@ -0,0 +1,35 @@
+import React, { Component, useState } from 'react';
+import dynamic from "next/dynamic";
+import { pdf, usePDF } from '@react-pdf/renderer';
+import Link from 'next/link';
+
+export default function ReactPdfSample() {
+  const Sample = dynamic(() => import("../Components/ReactPdf/sample"), {
+    ssr: false,
+  });
+  const BlobProvider = dynamic(() => import('@react-pdf/renderer'), {
+    ssr: false,
+  });
+
+  const Doc = (<Sample/>)
+
+  if (process.browser) {
+    // const [instance, setInstance] = usePDF({ document: Doc })
+    // console.log(instance)
+  }
+
+
+  const handleClick = () => {
+    const blob = pdf(Doc)
+    console.log('ok')
+  }
+
+  return <>
+    <div>
+      <Link href='/' style={{color: 'blue'}}>戻る</Link>
+      <h1>React PDF Sample</h1>
+      <Link href='https://react-pdf.org/' style={{color: 'blue'}}>React-pdfのリファレンス(v3を参照)</Link>
+      <Sample isPreview/>
+    </div>
+  </>
+}
